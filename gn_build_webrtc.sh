a0d85138d9421bc23e745133ac2bf064f3924db0
John Preston <johnprestonmail@gmail.com>
2020-05-18 17:48:00 +0400

    Fix build on Linux.

--- gn_build_webrtc.sh
+++ gn_build_webrtc.sh
@@ -1,32 +1,66 @@
 echo Configuring "out/Debug" build...
 
-ArgumentsList=`echo \
-    host_cpu=\"x64\" \
-    target_os=\"mac\" \
-    is_component_build=false \
-    is_debug=true \
-    symbol_level=2 \
-    proprietary_codecs=true \
-    use_custom_libcxx=false \
-    use_libjpeg_turbo=false \
-    use_xcode_clang=true \
-    use_rtti=true \
-    enable_iterator_debugging=true \
-    enable_dsyms=true \
-    rtc_include_tests=true \
-    rtc_build_examples=true \
-    rtc_build_tools=true \
-    rtc_build_opus=true \
-    rtc_build_ssl=false \
-    rtc_ssl_root=\"../../../openssl_1_1_1/include\" \
-    rtc_ssl_libs=[\"../../../openssl_1_1_1/libssl.a\",\"../../../openssl_1_1_1/libcrypto.a\"] \
-    rtc_builtin_ssl_root_certificates=true \
-    rtc_build_ffmpeg=false \
-    rtc_ffmpeg_root=\"../../../ffmpeg\" \
-    rtc_ffmpeg_libs=[\"../../../ffmpeg/libavutil/libavutil.a\",\"../../../ffmpeg/libavcodec/libavcodec.a\",\"../../../ffmpeg/libswscale/libswscale.a\",\"../../../ffmpeg/libswresample/libswresample.a\",\"/usr/local/macos/lib/libiconv.a\",\"CoreVideo.framework\"] \
-    rtc_enable_protobuf=false`
+if [[ "$OSTYPE" == "darwin"* ]]; then
+     ArgumentsList=`echo \
+        host_cpu=\"x64\" \
+        target_os=\"mac\" \
+        is_component_build=false \
+        is_debug=true \
+        symbol_level=2 \
+        proprietary_codecs=true \
+        use_custom_libcxx=false \
+        use_libjpeg_turbo=false \
+        use_xcode_clang=true \
+        use_rtti=true \
+        enable_iterator_debugging=true \
+        enable_dsyms=true \
+        rtc_include_tests=true \
+        rtc_build_examples=true \
+        rtc_build_tools=true \
+        rtc_build_opus=true \
+        rtc_build_ssl=false \
+        rtc_ssl_root=\"../../../openssl_1_1_1/include\" \
+        rtc_ssl_libs=[\"../../../openssl_1_1_1/libssl.a\",\"../../../openssl_1_1_1/libcrypto.a\"] \
+        rtc_builtin_ssl_root_certificates=true \
+        rtc_build_ffmpeg=false \
+        rtc_ffmpeg_root=\"../../../ffmpeg\" \
+        rtc_ffmpeg_libs=[\"../../../ffmpeg/libavcodec/libavcodec.a\",\"../../../ffmpeg/libswscale/libswscale.a\",\"../../../ffmpeg/libswresample/libswresample.a\",\"../../../ffmpeg/libavutil/libavutil.a\",\"/usr/local/macos/lib/libiconv.a\",\"CoreVideo.framework\"] \
+        rtc_enable_protobuf=false`
 
-gn gen out/Debug --ide=xcode --args="$ArgumentsList"
+   gn gen out/Debug --ide=xcode --args="$ArgumentsList"
+else
+    ArgumentsList=`echo \
+        host_cpu=\"x64\" \
+        target_os=\"linux\" \
+        is_component_build=false \
+        is_debug=true \
+        is_clang=false \
+        symbol_level=2 \
+        gtk_version=2 \
+        proprietary_codecs=true \
+        use_custom_libcxx=false \
+        use_libjpeg_turbo=false \
+        use_rtti=true \
+        use_gold=false \
+        use_sysroot=false \
+        linux_use_bundled_binutils=false \
+        enable_dsyms=true \
+        rtc_include_tests=true \
+        rtc_build_examples=true \
+        rtc_build_tools=true \
+        rtc_build_opus=true \
+        rtc_build_ssl=false \
+        rtc_ssl_root=\"../../../openssl_1_1_1/include\" \
+        rtc_ssl_libs=[\"../../../openssl_1_1_1/libssl.a\",\"../../../openssl_1_1_1/libcrypto.a\",\"/lib/x86_64-linux-gnu/libdl.so.2\",\"/usr/lib/x86_64-linux-gnu/libpthread.so\"] \
+        rtc_builtin_ssl_root_certificates=true \
+        rtc_build_ffmpeg=false \
+        rtc_ffmpeg_root=\"../../../ffmpeg\" \
+        rtc_ffmpeg_libs=[\"../../../ffmpeg/libavcodec/libavcodec.a\",\"../../../ffmpeg/libswscale/libswscale.a\",\"../../../ffmpeg/libswresample/libswresample.a\",\"../../../ffmpeg/libavutil/libavutil.a\",\"/usr/local/lib/libva-x11.a\",\"/usr/local/lib/libva-drm.a\",\"/usr/local/lib/libva.a\",\"/usr/local/lib/libvdpau.a\",\"/usr/lib/x86_64-linux-gnu/libdrm.a\",\"/usr/lib/x86_64-linux-gnu/libXfixes.a\",\"/usr/lib/x86_64-linux-gnu/libXext.a\",\"/usr/lib/x86_64-linux-gnu/libX11.a\",\"/usr/lib/x86_64-linux-gnu/libxcb.a\",\"/usr/lib/x86_64-linux-gnu/libXau.a\",\"/usr/lib/x86_64-linux-gnu/libXdmcp.a\",\"/usr/lib/x86_64-linux-gnu/libdbus-1.a\"] \
+        rtc_enable_protobuf=false`
+
+    gn gen out/Debug --args="$ArgumentsList"
+fi
+
 # We should use bundled:
 # rtc_build_opus=false
 # rtc_include_opus=false
